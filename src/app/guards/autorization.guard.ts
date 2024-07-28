0303eaf3204a116a71cef1da85b38ce0bf9a9488
Hamza JITTOU <jittouhamza@gmail.com>
2024-07-28 12:38:19 +0100

    first commit

---
+++ src/app/guards/autorization.guard.ts
@@ -0,0 +1,28 @@
+import {
+  ActivatedRouteSnapshot,
+  CanActivate, Router,
+  RouterStateSnapshot,
+  UrlTree
+} from '@angular/router';
+import {Observable} from "rxjs";
+import {Injectable} from "@angular/core";
+import {AppStateService} from "../services/app-state.service";
+
+@Injectable({
+  providedIn : 'root'
+})
+export class AutorizationGuard{
+  constructor(private appState : AppStateService,
+              private router : Router) {
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if(this.appState.authState.roles.includes(route.data['requiredRoles'])) {
+      return true;
+    } else {
+      this.router.navigateByUrl("/admin/notAuthorized");
+      return false;
+    }
+  }
+}
